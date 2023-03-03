ebeeb8681004daa3275ed1d59b91797a7acfb81c
Artem Golubev <golubevtema404@gmail.com>
2023-03-03 09:18:21 +0300

    Revert "update brain-even.js"
    This reverts commit c4ecc1bf9e9198b5bce8dfad3ca278b523745572.

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -3,37 +3,38 @@
 import readlineSync from 'readline-sync';
 import sayHello from '../src/cli.js';
 
-const name = sayHello();
-
 function generateNum() {
   const num = Math.ceil(Math.random() * 100 + Math.random() * 10);
   return num;
 }
 
-function playBrainEven() {
+function answerCheck(i, answer, isEven, name) {
+  let count = i;
+  if (answer.toLowerCase() === isEven) {
+    console.log('Correct!');
+    if (count === 3) {
+      console.log(`Congratulations, ${name}!`);
+    }
+    count += 1;
+    return count;
+  }
+  console.log(`${answer} is wrong answer ;(. Correct answer was ${isEven}.\nLet's try again, ${name}!`);
+  count = 0;
+  return count;
+}
+
+const brainEven = () => {
+  const name = sayHello();
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
   let i = 0;
   while (i < 3) {
     const number = generateNum();
     const isEven = number % 2 === 0 ? 'yes' : 'no';
     const answer = readlineSync.question(`Question: ${number}\n`);
 
-    if (answer.toLowerCase() === isEven) {
-      console.log('Correct!');
-      i += 1;
-    } else {
-      console.log(`${answer} is wrong answer ;(. Correct answer was ${isEven}.\nLet's try again, ${name}!`);
-      i = 0;
-    }
-    if (i === 3) {
-      console.log(`Congratulations, ${name}!`);
-    }
+    i = answerCheck(i, answer, isEven, name);
   }
-}
-
-const brainEven = () => {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-  playBrainEven();
 };
 
 brainEven();
